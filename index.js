f134bcdae826cbb7b18529172cd4559dc7152398
jasonl375 <144473636+jasonl375@users.noreply.github.com>
2024-10-18 00:14:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// Title: yaya
+var num = 0
+var button1 = document.getElementById("count-el")
+
+button2.innerHTML = num
+var button2 = document.getElementById("save-btn")
+
+function increment() {
+    num ++;
+    button1.innerHTML = num;
+    console.log(num);
+}
+
+function save() {
+    document.getElementById("entries").innerHTML += num + " - ";
+}
